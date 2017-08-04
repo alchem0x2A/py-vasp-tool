2da4f35f02344de577d6e27bedd73f250e419845
Tian Tian <lovaulonzezoeta@gmail.com>
2017-08-04 16:24:29 +0100

    Add VaspBandStructure

--- vasp_tool/other_vasp.py
+++ vasp_tool/other_vasp.py
@@ -6,6 +6,7 @@
 from ase.calculators.vasp import Vasp
 from ase.units import GPa
 from pymatgen.io.vasp import Vasprun, Outcar
+from ase.dft.kpoints import ibz_points, get_bandpath
 
 # Some fix for importing
 import sys
@@ -48,6 +49,78 @@ class VaspRelax(Vasp):
                       track_output=track_output,
                       **default_params)
 
+class VaspGround(Vasp):         # Calculate the ground state, always restart
+    def __init__(self, restart=True, output_template="vasp",
+                 track_output=False, **kwargs):
+        # First generate using normal Vasp class taking default params
+        # Overwrite the parameters by user default
+        default_params = {"istart": 0,  # Start from scratch
+                          "icharg": 2,  # should be 2 for relaxation
+                          "amix": 0.1,   # mixing parameters
+                          "bmix": 0.01,
+                          "ismear": 0,  # Gaussian smear
+                          "sigma": 0.01,  # smearing strength
+                          "ediff": 1e-8,  # energy difference
+                          "ediffg": 1e-7,
+                          "prec": "Accurate",  # precision
+                          "lwave": True,      # Do not store wave function
+                          "lcharg": True,    # Do not store the charge density
+                          "lvtot": False, # Do not store the local potential
+                          "encut": 800, # energy cutoff
+                          "nelm": 500,  # max SC steps
+                          "nelmin": 4,  # min SC steps
+                          "ibrion": -1,  # do relaxation
+                          "xc": "pbe",  # use PBE
+        }
+        # You should provide the kpoints line for yourself!
+        for key in kwargs:
+            default_params[key] = kwargs[key]
+
+        Vasp.__init__(self, restart=restart,
+                      output_template=output_template,
+                      track_output=track_output,
+                      **default_params)
+
+class VaspBandStructure(Vasp):         # Calculate the ground state, always restart
+    def __init__(self, restart=True, output_template="vasp",
+                 track_output=False, kpath=None,
+                 lattice_type=None, **kwargs):
+        # First generate using normal Vasp class taking default params
+        # Overwrite the parameters by user default
+        default_params = {"istart": 1,  # Start from scratch
+                          "icharg": 11,  # Read the charge density
+                          "amix": 0.1,   # mixing parameters
+                          "bmix": 0.01,
+                          "ismear": 0,  # Gaussian smear
+                          "sigma": 0.01,  # smearing strength
+                          "ediff": 1e-8,  # energy difference
+                          "ediffg": 1e-7,
+                          "prec": "Accurate",  # precision
+                          "lwave": True,      # Do not store wave function
+                          "lcharg": True,    # Do not store the charge density
+                          "lvtot": False, # Do not store the local potential
+                          "encut": 800, # energy cutoff
+                          "nelm": 500,  # max SC steps
+                          "nelmin": 4,  # min SC steps
+                          "ibrion": -1,  # do relaxation
+                          "xc": "pbe0",
+                          "lorbit": 11,   # write DOSCAR and PROCAR
+                          "algo": "exact"  # Diagonization
+                          "kpts_nintersections": 10,
+                          "reciprocal": True,
+        }
+        for key in kwargs:
+            default_params[key] = kwargs[key]
+
+        if (kpath is not None) and (lattice_type is not None):
+            kpts = gen_line_path(kpath, lattice_type)
+            default_params["kpts"] = kpts
+            
+
+        Vasp.__init__(self, restart=restart,
+                      output_template=output_template,
+                      track_output=track_output,
+                      **default_params)
 
 
 
